53ad819c440ac6d1fc22fbb46d5932ddb490f2a7
Piotr Kierzniewski <p.kierzniewski@viewone.pl>
2017-10-31 18:58:13 +0000

    Add date interval type field

--- tests/class-date-time-test.php
+++ tests/class-date-time-test.php
@@ -13,6 +13,7 @@ use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Form\FormInterface;
 
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\DateIntervalType;
 
 use Symfony\Component\Templating\PhpEngine;
 
@@ -77,5 +78,179 @@ class Date_Time_Test extends WP_UnitTestCase {
 		$input = $crawler->filter( 'form > #form > .field > .field__label + .field__input > #form_date' );
 
 		$this->assertEquals( 'date', $input->attr( 'type' ) );
+	}
+
+	/**
+	 * Test rendering choice date interval type field
+	 */
+	function test_rendering_choice_date_interval_type_field() {
+
+		$factory = mf_get_factory();
+		$form = $factory->create();
+
+		$form->add( 'interval', DateIntervalType::class, array(
+			'widget' => 'choice',
+			'with_days' => true,
+			'with_hours' => true,
+			'with_minutes' => true,
+			'with_months' => true,
+			'with_seconds' => true,
+			'with_years' => true,
+		) );
+
+		$form_view = $form->createView();
+
+		$engine = mf_get_engine();
+
+		$crawler = new Crawler( $engine['form']->form( $form_view ) );
+
+		$selects = $crawler->filter( 'form > #form > .field > .field__label + .field__input > #form_interval' );
+
+		$this->assertEquals( 6, count( $selects->children() ) );
+
+		$this->assertEquals( 'select', $selects->filter( '#form_interval_seconds' )->nodeName() );
+		$this->assertEquals( 'select', $selects->filter( '#form_interval_minutes' )->nodeName() );
+		$this->assertEquals( 'select', $selects->filter( '#form_interval_hours' )->nodeName() );
+		$this->assertEquals( 'select', $selects->filter( '#form_interval_days' )->nodeName() );
+		$this->assertEquals( 'select', $selects->filter( '#form_interval_months' )->nodeName() );
+		$this->assertEquals( 'select', $selects->filter( '#form_interval_years' )->nodeName() );
+
+		$this->assertEquals( 'form[interval][seconds]', $selects->filter( '#form_interval_seconds' )->attr( 'name' ) );
+		$this->assertEquals( 'form[interval][minutes]', $selects->filter( '#form_interval_minutes' )->attr( 'name' ) );
+		$this->assertEquals( 'form[interval][hours]', $selects->filter( '#form_interval_hours' )->attr( 'name' ) );
+		$this->assertEquals( 'form[interval][days]', $selects->filter( '#form_interval_days' )->attr( 'name' ) );
+		$this->assertEquals( 'form[interval][months]', $selects->filter( '#form_interval_months' )->attr( 'name' ) );
+		$this->assertEquals( 'form[interval][years]', $selects->filter( '#form_interval_years' )->attr( 'name' ) );
+	}
+
+	/**
+	 * Test rendering text date interval type field
+	 */
+	function test_rendering_text_date_interval_type_field() {
+
+		$factory = mf_get_factory();
+		$form = $factory->create();
+
+		$form->add( 'interval', DateIntervalType::class, array(
+			'widget' => 'text',
+			'with_days' => true,
+			'with_hours' => true,
+			'with_minutes' => true,
+			'with_months' => true,
+			'with_seconds' => true,
+			'with_years' => true,
+		) );
+
+		$form_view = $form->createView();
+
+		$engine = mf_get_engine();
+
+		$crawler = new Crawler( $engine['form']->form( $form_view ) );
+
+		$selects = $crawler->filter( 'form > #form > .field > .field__label + .field__input > #form_interval' );
+
+		$this->assertEquals( 6, count( $selects->children() ) );
+
+		$this->assertEquals( 'input', $selects->filter( '#form_interval_seconds' )->nodeName() );
+		$this->assertEquals( 'input', $selects->filter( '#form_interval_minutes' )->nodeName() );
+		$this->assertEquals( 'input', $selects->filter( '#form_interval_hours' )->nodeName() );
+		$this->assertEquals( 'input', $selects->filter( '#form_interval_days' )->nodeName() );
+		$this->assertEquals( 'input', $selects->filter( '#form_interval_months' )->nodeName() );
+		$this->assertEquals( 'input', $selects->filter( '#form_interval_years' )->nodeName() );
+
+		$this->assertEquals( 'text', $selects->filter( '#form_interval_seconds' )->attr( 'type' ) );
+		$this->assertEquals( 'text', $selects->filter( '#form_interval_minutes' )->attr( 'type' ) );
+		$this->assertEquals( 'text', $selects->filter( '#form_interval_hours' )->attr( 'type' ) );
+		$this->assertEquals( 'text', $selects->filter( '#form_interval_days' )->attr( 'type' ) );
+		$this->assertEquals( 'text', $selects->filter( '#form_interval_months' )->attr( 'type' ) );
+		$this->assertEquals( 'text', $selects->filter( '#form_interval_years' )->attr( 'type' ) );
+
+		$this->assertEquals( 'form[interval][seconds]', $selects->filter( '#form_interval_seconds' )->attr( 'name' ) );
+		$this->assertEquals( 'form[interval][minutes]', $selects->filter( '#form_interval_minutes' )->attr( 'name' ) );
+		$this->assertEquals( 'form[interval][hours]', $selects->filter( '#form_interval_hours' )->attr( 'name' ) );
+		$this->assertEquals( 'form[interval][days]', $selects->filter( '#form_interval_days' )->attr( 'name' ) );
+		$this->assertEquals( 'form[interval][months]', $selects->filter( '#form_interval_months' )->attr( 'name' ) );
+		$this->assertEquals( 'form[interval][years]', $selects->filter( '#form_interval_years' )->attr( 'name' ) );
+	}
+
+	/**
+	 * Test rendering integer date interval type field
+	 */
+	function test_rendering_integer_date_interval_type_field() {
+
+		$factory = mf_get_factory();
+		$form = $factory->create();
+
+		$form->add( 'interval', DateIntervalType::class, array(
+			'widget' => 'integer',
+			'with_days' => true,
+			'with_hours' => true,
+			'with_minutes' => true,
+			'with_months' => true,
+			'with_seconds' => true,
+			'with_years' => true,
+		) );
+
+		$form_view = $form->createView();
+
+		$engine = mf_get_engine();
+
+		$crawler = new Crawler( $engine['form']->form( $form_view ) );
+
+		$selects = $crawler->filter( 'form > #form > .field > .field__label + .field__input > #form_interval' );
+
+		$this->assertEquals( 6, count( $selects->children() ) );
+
+		$this->assertEquals( 'input', $selects->filter( '#form_interval_seconds' )->nodeName() );
+		$this->assertEquals( 'input', $selects->filter( '#form_interval_minutes' )->nodeName() );
+		$this->assertEquals( 'input', $selects->filter( '#form_interval_hours' )->nodeName() );
+		$this->assertEquals( 'input', $selects->filter( '#form_interval_days' )->nodeName() );
+		$this->assertEquals( 'input', $selects->filter( '#form_interval_months' )->nodeName() );
+		$this->assertEquals( 'input', $selects->filter( '#form_interval_years' )->nodeName() );
+
+		$this->assertEquals( 'number', $selects->filter( '#form_interval_seconds' )->attr( 'type' ) );
+		$this->assertEquals( 'number', $selects->filter( '#form_interval_minutes' )->attr( 'type' ) );
+		$this->assertEquals( 'number', $selects->filter( '#form_interval_hours' )->attr( 'type' ) );
+		$this->assertEquals( 'number', $selects->filter( '#form_interval_days' )->attr( 'type' ) );
+		$this->assertEquals( 'number', $selects->filter( '#form_interval_months' )->attr( 'type' ) );
+		$this->assertEquals( 'number', $selects->filter( '#form_interval_years' )->attr( 'type' ) );
+
+		$this->assertEquals( 'form[interval][seconds]', $selects->filter( '#form_interval_seconds' )->attr( 'name' ) );
+		$this->assertEquals( 'form[interval][minutes]', $selects->filter( '#form_interval_minutes' )->attr( 'name' ) );
+		$this->assertEquals( 'form[interval][hours]', $selects->filter( '#form_interval_hours' )->attr( 'name' ) );
+		$this->assertEquals( 'form[interval][days]', $selects->filter( '#form_interval_days' )->attr( 'name' ) );
+		$this->assertEquals( 'form[interval][months]', $selects->filter( '#form_interval_months' )->attr( 'name' ) );
+		$this->assertEquals( 'form[interval][years]', $selects->filter( '#form_interval_years' )->attr( 'name' ) );
+	}
+
+	/**
+	 * Test rendering single text date interval type field
+	 */
+	function test_rendering_single_text_date_interval_type_field() {
+
+		$factory = mf_get_factory();
+		$form = $factory->create();
+
+		$form->add( 'interval', DateIntervalType::class, array(
+			'widget' => 'single_text',
+			'with_days' => true,
+			'with_hours' => true,
+			'with_minutes' => true,
+			'with_months' => true,
+			'with_seconds' => true,
+			'with_years' => true,
+		) );
+
+		$form_view = $form->createView();
+
+		$engine = mf_get_engine();
+
+		$crawler = new Crawler( $engine['form']->form( $form_view ) );
+
+		$selects = $crawler->filter( 'form > #form > .field > .field__label + .field__input > #form_interval' );
+
+		$this->assertEquals( 'input', $selects->filter( '#form_interval' )->nodeName() );
+		$this->assertEquals( 'text', $selects->filter( '#form_interval' )->attr( 'type' ) );
+		$this->assertEquals( 'form[interval]', $selects->filter( '#form_interval' )->attr( 'name' ) );
 	}
 }
